115e0bf2c1af8ad25fb3a11d3536bae282462892
PrasannaBG <bgprez123@gmail.com>
2023-02-05 16:08:27 +0530

    Added 2D_Array_Sum

---
+++ Arrays/2D_List_Sum(Richest_Customer_Wealth).py
@@ -0,0 +1,18 @@
+def maximumWealth(self, accounts):
+        # Count = []
+        # for i in accounts:
+        #    Count.append(sum(i))
+        # return max(Count)
+        
+        Max = 0
+        for i in range(len(accounts)):
+            Sum = 0
+            for j in range(len(accounts[i])):
+                Sum = Sum + accounts[i][j]
+            if Sum > Max:
+                Max = Sum
+        return Max
+
+'''
+Declare a Temp variable to add inner list's numbers and 
+check each list sum is greater than max or not in each iteration '''
